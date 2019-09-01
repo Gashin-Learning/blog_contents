eea90e05d8e0cc0307f91403bf6c230eeeca4559
Gashin-Learning <54409085+Gashin-Learning@users.noreply.github.com>
2019-09-01 23:24:30 +0900

    Add files via upload

---
+++ 002_nonparaBayes_3D_IRM_gibbs_sampling/data_create.py
@@ -0,0 +1,50 @@
+import numpy as np
+from collections import Counter
+import seaborn as sns
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+def make_toy_data(theta, x_dim, y_dim, z_dim, plot_option=True):
+
+    # set parameter of bernouli distributions
+    theta=theta
+
+    R_raw = np.concatenate([np.concatenate([np.concatenate([np.random.binomial(1, p_ijk, size=(x_dim,y_dim,z_dim)) for p_ijk in p_ij], axis=2) for p_ij in p_i], axis=1) for p_i in theta])
+
+    # shuffle x y z
+    R_X_index = np.random.permutation(np.arange(R_raw.shape[0]))
+    R_Y_index = np.random.permutation(np.arange(R_raw.shape[1]))
+    R_Z_index = np.random.permutation(np.arange(R_raw.shape[2]))
+    R = R_raw[R_X_index,:,:]
+    R = R[:, R_Y_index, :]
+    R = R[:, :, R_Z_index]
+
+    if plot_option:
+        fig = plt.figure(figsize=(14,5))
+        ax0 = fig.add_subplot(121, projection='3d')
+        for x in range(R_raw.shape[0]):
+
+            R_raw_per_x = R_raw[x,:,:]
+            x1=np.arange(R_raw_per_x.shape[1])
+            x2=np.arange(R_raw_per_x.shape[0])
+            X1, X2 = np.meshgrid(x1, x2)
+            X1_on = np.ravel(X1)[np.ravel(R_raw_per_x)==1]
+            X2_on = np.ravel(X2)[np.ravel(R_raw_per_x)==1]
+            ax0.scatter3D(np.ravel(X1_on), np.ravel(X2_on), x, marker="o", linestyle='None', color='purple')
+
+        ax1 = fig.add_subplot(122, projection='3d')
+        for x in range(R.shape[0]):
+            R_per_x = R[x,:,:]
+            x1=np.arange(R_per_x.shape[1])
+            x2=np.arange(R_per_x.shape[0])
+            X1, X2 = np.meshgrid(x1, x2)
+            X1_on = np.ravel(X1)[np.ravel(R_per_x)==1]
+            X2_on = np.ravel(X2)[np.ravel(R_per_x)==1]
+
+            ax1.scatter3D(np.ravel(X1_on), np.ravel(X2_on), x, marker="o",linestyle='None', color='purple')
+
+        ax0.set_title("R_raw", fontsize=15)
+        ax1.set_title("Shuffled as Original data", fontsize=15);
+
+    return R
