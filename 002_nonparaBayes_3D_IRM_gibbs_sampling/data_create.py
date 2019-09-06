501d3590ae05f4fbfc051b92f592327c3f52553f
Gashin-Learning <54409085+Gashin-Learning@users.noreply.github.com>
2019-09-06 20:58:41 +0900

    Add files via upload

--- 002_nonparaBayes_3D_IRM_gibbs_sampling/data_create.py
+++ 002_nonparaBayes_3D_IRM_gibbs_sampling/data_create.py
@@ -5,8 +5,9 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 
-def make_toy_data(theta, x_dim, y_dim, z_dim, plot_option=True):
+def make_toy_data(theta, x_dim, y_dim, z_dim, plot_option=True, my_seed=0):
 
+    np.random.seed(my_seed)
     # set parameter of bernouli distributions
     theta=theta
 
@@ -20,7 +21,7 @@ def make_toy_data(theta, x_dim, y_dim, z_dim, plot_option=True):
     R = R[:, R_Y_index, :]
     R = R[:, :, R_Z_index]
 
-    if plot_option:
+    if plot_option=='scatter':
         fig = plt.figure(figsize=(14,5))
         ax0 = fig.add_subplot(121, projection='3d')
         for x in range(R_raw.shape[0]):
@@ -46,5 +47,26 @@ def make_toy_data(theta, x_dim, y_dim, z_dim, plot_option=True):
 
         ax0.set_title("R_raw", fontsize=15)
         ax1.set_title("Shuffled as Original data", fontsize=15);
+
+    else:
+        fig = plt.figure(figsize=(16,6))
+
+        colors = np.where(R_raw==1,'#00bfffC0', '#00000000')
+        voxels1=R_raw==1
+        voxels2=R_raw==0
+        ax0 = fig.add_subplot(121, projection='3d')
+        ax0 = fig.gca(projection='3d')
+        ax0.voxels(voxels1, facecolors=colors, edgecolor='#00008b73')
+        ax0.voxels(voxels2, facecolors=colors, edgecolor='#ff8c0073')
+        ax0.set_title("Raw data", fontsize=20)
+
+        colors = np.where(R==1,'#00bfffC0', '#00000000')
+        voxels1=R==1
+        voxels2=R==0
+        ax1 = fig.add_subplot(122, projection='3d')
+        ax1 = fig.gca(projection='3d')
+        ax1.voxels(voxels1, facecolors=colors, edgecolor='#00008b73')
+        ax1.voxels(voxels2, facecolors=colors, edgecolor='#ff8c0073')
+        ax1.set_title("Shuffled", fontsize=20);
 
     return R
